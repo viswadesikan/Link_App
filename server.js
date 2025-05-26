71a9d38a334834ab42c6ef6c720983ec91b19b0a
Santhoshmfai <santhoshraj.mfai@gmail.com>
2025-05-26 09:43:46 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const authRoutes = require('./routes/authRoutes');
@@ -15,7 +16,24 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Serve static CSS/JS
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+// Serve HTML files manually
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/auth', (req, res) => {
+  res.sendFile(path.join(__dirname, 'auth.html'));
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', linkRoutes);
 
