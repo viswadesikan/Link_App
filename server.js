bd402cc6d57cc5bf6b510db2faa5eac976c411ea
Santhoshmfai <santhoshraj.mfai@gmail.com>
2025-05-26 11:38:25 +0530

    added heaalth API

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.get('/home', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api', linkRoutes);
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
